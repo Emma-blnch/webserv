64b1f82250ceaa861c02a5184328d895255ca2ae
Emma Blanchard <eblancha@f1br10s7.clusters.42paris.fr>
2025-05-19 12:15:56 +0200

    handling of cgi completed with good args + main handling multiple server)

--- http/handleCgi.cpp
+++ http/handleCgi.cpp
@@ -1,10 +1,50 @@
 #include "handleCgi.hpp"
 
-void Response::handleCGI(const Request& req, const LocationBlock* location) {
+std::vector<std::string> Response::buildCGIEnvp(const Request& req) {
+    std::vector<std::string> envs;
+    envs.push_back("GATEWAY_INTERFACE=CGI/1.1");
+    envs.push_back("SERVER_PROTOCOL=HTTP/1.1");
+    envs.push_back("REQUEST_METHOD=" + req.getMethod());
+    envs.push_back("SCRIPT_NAME=" + req.getPath());
+    envs.push_back("QUERY_STRING=" + req.getQuery());
+
+    std::string contentLength = req.getHeader("Content-Length");
+    if (!contentLength.empty())
+        envs.push_back("CONTENT_LENGTH=" + contentLength);
+    std::string contentType = req.getHeader("Content-Type");
+    if (!contentType.empty())
+        envs.push_back("CONTENT_TYPE=" + contentType);
+    return envs;
+}
+
+std::string Response::readCGIOutput(int fd) {
+    char buffer[1024];
+    std::string output;
+    ssize_t bytesRead;
+    while ((bytesRead = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
+        buffer[bytesRead] = '\0';
+        output += buffer;
+    }
+    close(fd);
+    return output;
+}
+
+void Response::handleCGI(const Request& req, const LocationBlock* location, const ServerBlock& server) {
     // Verifier method allowed dans location block dans config file
     if (!isMethodAllowed(location, req.getMethod())) {
         setStatus(405);
         return;
+    }
+    // Verifier taille du body
+    if (req.getMethod() == "POST") {
+        if (!checkContentLength(req.getBody(), req))
+            return;
+        size_t maxSize = location ? location->maxBodySize : server.getClientMaxBodySize();
+        if (maxSize > 0 && req.getBody().size() > maxSize) {
+            setStatus(413);
+            setBody("413 Payload Too Large");
+            return;
+        }
     }
     // recup interpreteur dans location block
     if (!location || location->cgiPath.empty()) {
@@ -28,17 +68,19 @@ void Response::handleCGI(const Request& req, const LocationBlock* location) {
         scriptPath = ".." + req.getPath();
     }
     // vérifier que le script existe et est exécutable
-    if (access(scriptPath.c_str(), F_OK) != 0) {
-        setStatus(404);
-        return;
-    }
-    if (access(scriptPath.c_str(), X_OK) != 0) {
-        setStatus(403);
-        return;
-    }
+    if (!checkFilePermissions(scriptPath, F_OK, 404)) return;
+    if (!checkFilePermissions(scriptPath, X_OK, 403)) return;
     // créer un pipe pour récupérer la sortie du script
     int pipefd[2];
     if (pipe(pipefd) == -1) {
+        setStatus(500);
+        return;
+    }
+    // pipe pour entree (body quand methode POST)
+    int inputPipe[2];
+    if (req.getMethod() == "POST"  && pipe(inputPipe) == -1) {
+        close(pipefd[0]);
+        close(pipefd[1]);
         setStatus(500);
         return;
     }
@@ -48,39 +90,45 @@ void Response::handleCGI(const Request& req, const LocationBlock* location) {
         return;
     }
     else if (pid == 0) { // CHILD
-        // redirige STDOUT vers pipe
+        // redirige pipe sur STDOUT
         dup2(pipefd[1], STDOUT_FILENO);
         close(pipefd[0]); // ferme lecture
         close(pipefd[1]);
-
+        if (req.getMethod() == "POST") {
+            dup2(inputPipe[0], STDIN_FILENO); // redirige pipe sur STDIN
+            close(inputPipe[0]);
+            close(inputPipe[1]);
+        }
+        // construire variables d'env
+        std::string method = req.getMethod();
+        std::string query = req.getQuery();
+        std::string contentLength = req.getHeader("Content-Length");
+        std::string contentType = req.getHeader("Content-Type");
+        std::vector<std::string> envs = buildCGIEnvp(req);
+        // conversion en char*[] pour execve
+        std::vector<char*> envp;
+        for (size_t i = 0; i < envs.size(); ++i)
+            envp.push_back(const_cast<char*>(envs[i].c_str ()));
+        envp.push_back(NULL);
         // prépare args pour execve
         char* const argv[] = {
             (char*)interpreter.c_str(),          // interpréteur
             (char*)scriptPath.c_str(),          // script à exécuter
             NULL
         };
-        // prépare variables d'environnement minimales
-        char* const envp[] = {
-            (char*)"GATEWAY_INTERFACE=CGI/1.1",
-            (char*)"SERVER_PROTOCOL=HTTP/1.1",
-            (char*)"REQUEST_METHOD=GET",
-            NULL
-        };
-        execve("/usr/bin/python3", argv, envp);
-        // si exec échoue
+        execve(interpreter.c_str(), argv, &envp[0]);
+        // si execve échoue
         exit(1);
     }
     else { // PARENT
         close(pipefd[1]); // lit la sortie du script
-        char buffer[1024];
-        std::string output;
-
-        ssize_t bytesRead;
-        while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0) {
-            buffer[bytesRead] = '\0';
-            output += buffer;
+        if (req.getMethod() == "POST") {
+            close(inputPipe[0]);
+            // ecrit le body dans entree du cgi
+            write(inputPipe[1], req.getBody().c_str(), req.getBody().size());
+            close(inputPipe[1]);
         }
-        close(pipefd[0]);
+        std::string output = readCGIOutput(pipefd[0]);
         waitpid(pid, NULL, 0); // attend fin du processus enfant
         // préparer la réponse
         setStatus(200);
--- main.cpp
+++ main.cpp
@@ -16,52 +16,93 @@
 
 #define BUFFER_SIZE 8192
 
-int main() {
+struct SocketData {
+    int fd;
+    std::string host;
+    int port;
+    std::vector<const ServerBlock*> serverBlocks; // Tous les ServerBlocks pour ce host:port
+};
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: ./webserv [CONFIG FILE].conf" << std::endl;
+        return 1;
+    }
     // parser fichier de config
     ConfigFile config;
-    if (!config.parseConfigFile("file.conf")) {
+    if (!config.parseConfigFile(argv[1])) {
         std::cerr << "Erreur parsing config.\n";
         return 1;
     }
-
+    // recuperer tous les bloc server
     std::vector<ServerBlock> servers = config.getServers();
     if (servers.empty()) {
         std::cerr << "Aucun bloc serveur valide.\n";
         return 1;
     }
-
-    const ServerBlock& server = servers[0];
-
-    // créer une socket serveur
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket < 0) {
-        std::cerr << "Socket creation failed\n";
-        return 1;
+    // stocker tous les socket ouverts
+    std::vector<SocketData> sockets;
+    // boucle pour recup tous les host port de chaque server
+    for (size_t i = 0; i < servers.size(); ++i) {
+        // server actuel
+        const ServerBlock& server = servers[i];
+        std::string host = server.getHost();
+        int port = server.getPort();
+        // creation socker server pour ce server
+        int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (serverSocket < 0) {
+            std::cerr << "Socket creation failed for " << host << ":" << port << std::endl;
+            return 1;
+        }
+        // configurer son adresse
+        sockaddr_in serverAddress;
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(port);
+        std::cout << "port " << server.getPort() << std::endl;
+        serverAddress.sin_addr.s_addr = inet_addr(host.c_str());
+        std::cout << "host " << server.getHost() << std::endl;
+        // bind et listen
+        if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+            std::cerr << "Bind failed for " << host << ":" << port << std::endl;
+            close(serverSocket);
+            continue;
+        }
+        if (listen(serverSocket, 5) < 0) {
+            std::cerr << "Listen failed for " << host << ":" << port << std::endl;
+            close(serverSocket);
+            continue;
+        }
+        // check si deja socket ouverte pour ce host:port
+        bool found = false;
+        for (size_t j = 0; j < sockets.size(); ++j) {
+            if (sockets[j].host == host && sockets[j].port == port) {
+                sockets[j].serverBlocks.push_back(&server);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            SocketData data;
+            data.fd = serverSocket;
+            data.host = host;
+            data.port = port;
+            data.serverBlocks.push_back(&server);
+            sockets.push_back(data);
+        }
     }
-
-    // configurer son adresse
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(server.getPort());
-    std::cout << "port " << server.getPort() << std::endl;
-    serverAddress.sin_addr.s_addr = inet_addr(server.getHost().c_str());
-    std::cout << "host " << server.getHost() << std::endl;
-    // serverAddress.sin_port = htons(8080);
-    // serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    // bind le socket à un certain port
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-
-    // écouter les connexions
-    listen(serverSocket, 5);
-
     // préparer poll avec socket serveur
     std::vector<struct pollfd> fds;
-    struct pollfd serverFd;
-    serverFd.fd = serverSocket;
-    serverFd.events = POLLIN; // = "préviens moi quand il ya des connexions"
-    fds.push_back(serverFd);
+    // chaque fd dans fds est un socket server avec host:port
+    for (size_t i = 0; i < sockets.size(); ++i) {
+        struct pollfd serverFd;
+        serverFd.fd = sockets[i].fd;
+        serverFd.events = POLLIN; // = "préviens moi quand il ya des connexions"
+        fds.push_back(serverFd);
+    }
 
+    // faire la correspondance clientSocket -> index socket serveur utiliser
+    std::map<int, size_t> clientSocketToServer;
+    
     char buffer[BUFFER_SIZE + 1];
 
     while (true) {
@@ -73,60 +114,205 @@ int main() {
 
         // check qui a causé l'activité
         for (size_t i = 0; i < fds.size(); ++i) {
-            if (fds[i].revents & POLLIN) { // revents contient évènements détectés
-                // si c serverSocket alors nouvelle connexion
-                if (fds[i].fd == serverSocket) {
-                    int clientSocket = accept(serverSocket, NULL, NULL);
-                    if (clientSocket >= 0) {
-                        struct pollfd clientFd;
-                        clientFd.fd = clientSocket;
-                        clientFd.events = POLLIN;
-                        fds.push_back(clientFd);
-                    }
+            bool isServerFd = false;
+            size_t serverIndex = 0;
+            // si c serverSocket alors nouvelle connexion
+            for (; serverIndex < sockets.size(); ++serverIndex) {
+                if (fds[i].fd == sockets[serverIndex].fd && (fds[i].revents & POLLIN)) {
+                    isServerFd = true;
+                    break;
                 }
-                // si c un client existant je lis sa requête
-                else {
-                    int clientFd = fds[i].fd;
-                    int bytesRead = recv(clientFd, buffer, BUFFER_SIZE, 0);
-                    if (bytesRead <= 0) {
-                        std::cout << "Client disconnected\n";
-                        close(clientFd);
-                        fds.erase(fds.begin() + i);
-                        --i;
-                        continue;
-                    }
-                    buffer[bytesRead] = '\0';
-
-                    try {
-                        Request req;
-                        req.parseRawRequest(buffer);
-                
-                        Response res;
-                        res.setErrorPages(server.getErrorPages());
-                        res.buildFromRequest(req, server);
-                        std::string responseStr = res.returnResponse();
-                
-                        send(clientFd, responseStr.c_str(), responseStr.size(), 0);
-                    } catch (const std::exception& e) {
-                        std::cerr << "Erreur : " << e.what() << std::endl;
-                
-                        // Envoi réponse 400
-                        const char* errorResponse =
-                            "HTTP/1.1 400 Bad Request\r\n"
-                            "Content-Type: text/plain\r\n"
-                            "Content-Length: 11\r\n"
-                            "\r\n"
-                            "Bad Request";
-                        send(clientFd, errorResponse, strlen(errorResponse), 0);
-                    }
+            }
+            if (isServerFd) {
+                int clientSocket = accept(fds[i].fd, NULL, NULL);
+                if (clientSocket >= 0) {
+                    struct pollfd clientFd;
+                    clientFd.fd = clientSocket;
+                    clientFd.events = POLLIN;
+                    fds.push_back(clientFd);
+                    clientSocketToServer[clientSocket] = serverIndex;
+                }
+                continue;
+            }
+            // si c un client existant je lis sa requête
+            if (fds[i].revents & POLLIN) {
+                int clientFd = fds[i].fd;
+                int bytesRead = recv(clientFd, buffer, BUFFER_SIZE, 0);
+                if (bytesRead <= 0) {
+                    std::cout << "Client disconnected\n";
                     close(clientFd);
                     fds.erase(fds.begin() + i);
                     --i;
+                    continue;
                 }
+                buffer[bytesRead] = '\0';
+
+                try {
+                    // trouver a quel socket correspond client
+                    size_t socketIndex = clientSocketToServer[clientFd];
+                    const std::vector<const ServerBlock*>& serverBlocks = sockets[socketIndex].serverBlocks;
+                    // parse request http
+                    Request req;
+                    req.parseRawRequest(buffer);
+                    // choix du bon serverBlock via Host header
+                    std::string hostHeader = req.getHeader("host");
+                    const ServerBlock* chosenServer = NULL;
+                    for (size_t s = 0; s < serverBlocks.size(); ++s) {
+                        if (serverBlocks[s]->getServerName() == hostHeader) {
+                            chosenServer = serverBlocks[s];
+                            break;
+                        }
+                    }
+                    if (!chosenServer)
+                        chosenServer = serverBlocks[0];
+
+                    Response res;
+                    res.setErrorPages(chosenServer->getErrorPages());
+                    res.buildFromRequest(req, *chosenServer);
+                    std::string responseStr = res.returnResponse();
+            
+                    send(clientFd, responseStr.c_str(), responseStr.size(), 0);
+                } catch (const std::exception& e) {
+                    std::cerr << "Erreur : " << e.what() << std::endl;
+            
+                    // Envoi réponse 400
+                    const char* errorResponse =
+                        "HTTP/1.1 400 Bad Request\r\n"
+                        "Content-Type: text/plain\r\n"
+                        "Content-Length: 11\r\n"
+                        "\r\n"
+                        "Bad Request";
+                    send(clientFd, errorResponse, strlen(errorResponse), 0);
+                }
+                close(clientFd);
+                fds.erase(fds.begin() + i);
+                clientSocketToServer.erase(clientFd);
+                --i;
             }
         }
     }
-
-    close(serverSocket);
+    for (size_t i = 0; i < sockets.size(); ++i)
+        close(sockets[i].fd);
     return 0;
 }
+
+// un seul server:
+
+// int main(int argc, char **argv) {
+//     if (argc < 2) {
+//         std::cerr << "Usage: ./webserv [CONFIG FILE].conf" << std::endl;
+//         return 1;
+//     }
+//     // parser fichier de config
+//     ConfigFile config;
+//     if (!config.parseConfigFile(argv[1])) {
+//         std::cerr << "Erreur parsing config.\n";
+//         return 1;
+//     }
+
+//     std::vector<ServerBlock> servers = config.getServers();
+//     if (servers.empty()) {
+//         std::cerr << "Aucun bloc serveur valide.\n";
+//         return 1;
+//     }
+
+//     const ServerBlock& server = servers[0];
+
+//     // créer une socket serveur
+//     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+//     if (serverSocket < 0) {
+//         std::cerr << "Socket creation failed\n";
+//         return 1;
+//     }
+
+//     // configurer son adresse
+//     sockaddr_in serverAddress;
+//     serverAddress.sin_family = AF_INET;
+//     serverAddress.sin_port = htons(server.getPort());
+//     std::cout << "port " << server.getPort() << std::endl;
+//     serverAddress.sin_addr.s_addr = inet_addr(server.getHost().c_str());
+//     std::cout << "host " << server.getHost() << std::endl;
+//     // serverAddress.sin_port = htons(8080);
+//     // serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+
+//     // bind le socket à un certain port
+//     bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+//     // écouter les connexions
+//     listen(serverSocket, 5);
+
+//     // préparer poll avec socket serveur
+//     std::vector<struct pollfd> fds;
+//     struct pollfd serverFd;
+//     serverFd.fd = serverSocket;
+//     serverFd.events = POLLIN; // = "préviens moi quand il ya des connexions"
+//     fds.push_back(serverFd);
+
+//     char buffer[BUFFER_SIZE + 1];
+
+//     while (true) {
+//         int pollCount = poll(&fds[0], fds.size(), -1);
+//         if (pollCount < 0) {
+//             std::cerr << "Poll error\n";
+//             break;
+//         }
+
+//         // check qui a causé l'activité
+//         for (size_t i = 0; i < fds.size(); ++i) {
+//             if (fds[i].revents & POLLIN) { // revents contient évènements détectés
+//                 // si c serverSocket alors nouvelle connexion
+//                 if (fds[i].fd == serverSocket) {
+//                     int clientSocket = accept(serverSocket, NULL, NULL);
+//                     if (clientSocket >= 0) {
+//                         struct pollfd clientFd;
+//                         clientFd.fd = clientSocket;
+//                         clientFd.events = POLLIN;
+//                         fds.push_back(clientFd);
+//                     }
+//                 }
+//                 // si c un client existant je lis sa requête
+//                 else {
+//                     int clientFd = fds[i].fd;
+//                     int bytesRead = recv(clientFd, buffer, BUFFER_SIZE, 0);
+//                     if (bytesRead <= 0) {
+//                         std::cout << "Client disconnected\n";
+//                         close(clientFd);
+//                         fds.erase(fds.begin() + i);
+//                         --i;
+//                         continue;
+//                     }
+//                     buffer[bytesRead] = '\0';
+
+//                     try {
+//                         Request req;
+//                         req.parseRawRequest(buffer);
+                
+//                         Response res;
+//                         res.setErrorPages(server.getErrorPages());
+//                         res.buildFromRequest(req, server);
+//                         std::string responseStr = res.returnResponse();
+                
+//                         send(clientFd, responseStr.c_str(), responseStr.size(), 0);
+//                     } catch (const std::exception& e) {
+//                         std::cerr << "Erreur : " << e.what() << std::endl;
+                
+//                         // Envoi réponse 400
+//                         const char* errorResponse =
+//                             "HTTP/1.1 400 Bad Request\r\n"
+//                             "Content-Type: text/plain\r\n"
+//                             "Content-Length: 11\r\n"
+//                             "\r\n"
+//                             "Bad Request";
+//                         send(clientFd, errorResponse, strlen(errorResponse), 0);
+//                     }
+//                     close(clientFd);
+//                     fds.erase(fds.begin() + i);
+//                     --i;
+//                 }
+//             }
+//         }
+//     }
+
+//     close(serverSocket);
+//     return 0;
+// }
